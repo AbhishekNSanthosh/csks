c0cf125d2fe25a81bc62bdb33828827abce98eba
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-06-01 22:43:41 +0530

    feat:footer added

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styles from "./Footer.module.css";
+import logo from "../../assets/logo.png";
+import { navbarLinks } from "../../Utils/Constants";
+
+export const Footer = () => {
+  return (
+    <div className={styles.footerContainer}>
+      <div className={styles.wrapper}>
+        <div className={styles.row}>
+          <div className={styles.col1}>
+            <img src={logo} alt="" className={styles.logo} />
+          </div>
+          <div className={styles.col2}>
+            <span className={styles.title}>Useful links</span>
+            <div className={styles.linkBox}>
+              {navbarLinks?.map((link) => (
+                <div className={styles.linkWrap}>
+                  <a href="" className={styles.link}>
+                    {link?.title}
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className={styles.col2}>
+            <span className={styles.title}>Other Links</span>
+            <div className={styles.linkBox}>
+              {navbarLinks?.map((link) => (
+                <div className={styles.linkWrap}>
+                  <a href="" className={styles.link}>
+                    {link?.title}
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className={styles.hr}></div>
+        <div className={styles.row}>
+            <div className={styles.left}>
+                <span className={styles.credits}>All Rights Reserved</span>
+            </div>
+            <div className={styles.left}>
+                <span className={styles.credits}>Crafted by Web Team IEEE CSKS</span>
+            </div>
+            <div className={styles.left}>
+            <span className={styles.credits}>© Copyright | IEEE CS KS</span>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "./Home.module.css";
 import { Navbar } from "../../Components/Navbar/Navbar";
 import { Banner } from "./Components/Banner/Banner";
+import { Footer } from "../../Components/Footer/Footer";
 
 export const Home = () => {
   return (
     <>
       <Navbar />
       <Banner/>
+      <Footer/>
     </>
   );
 };
