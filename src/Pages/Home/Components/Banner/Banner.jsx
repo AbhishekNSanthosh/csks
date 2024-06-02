48e096b7df21ee2b1c8f4745cacfe52879ce7919
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-06-02 07:31:54 +0530

    refactor:background image

--- src/Pages/Home/Components/Banner/Banner.jsx
+++ src/Pages/Home/Components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ import bannerImg from '../../../../assets/banner.svg'
 export const Banner = () => {
   return (
     <div className={styles.bannerContainer}>
-    <img src={bannerImg} alt="" className={styles.bannerImg}/>
+    {/* <img src={bannerImg} alt="" className={styles.bannerImg}/> */}
     <div className={styles.dataBox}>
         <span className={styles.bannerTitle}>Belong Where Technology Innovators Thrive</span>
         <span className={styles.subTitle}>Become part of the community of computing leaders driving the future of technology.</span>
