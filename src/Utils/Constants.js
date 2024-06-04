e24d5960a450fb194c5776ec792629be31b5ff45
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-06-04 17:56:20 +0530

    feat:execom photos added

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -1,3 +1,25 @@
+import Abhirami from '../assets/execom/Abhirami.png'
+import Abhishek from '../assets/execom/Abhishek.png'
+import Biswas from '../assets/execom/BiswasSekhar.png'
+import Deepthi from '../assets/execom/drdeepthis.png'
+import Janajith from '../assets/execom/Janajith.png'
+import Justin from '../assets/execom/Justin.png'
+import Jyotish from '../assets/execom/Jyotish.png'
+import Maria from '../assets/execom/MARIABINU.png'
+import Akhil from '../assets/execom/mrakhilraj.png'
+import Devanarayanan from '../assets/execom/mrdevanarayanansv.png' 
+import Karthik from '../assets/execom/mrkarthik.png'
+import AnnaMathew from '../assets/execom/msannamathew.png'
+import Nathan from '../assets/execom/Nathan.png'
+import Neha from '../assets/execom/Neha.png'
+import Sarju from '../assets/execom/profsarjus.png'
+import Robin from '../assets/execom/Robin.png'
+import Sabareesh from '../assets/execom/Sabareesh1.png'
+import Satheesh from '../assets/execom/SatheeshKumar.png'
+import Swsthik from '../assets/execom/Swsthik.png'
+import Shiraz from '../assets/execom/Shiraz.png'
+import Vasif from '../assets/execom/vasif.png'
+
 
 export const navbarLinks = [
     {
@@ -32,3 +54,179 @@ export const OtherLinks = [
         link:"https://www.computer.org/",
     },
 ]
+
+export const Execom24 =  [
+    {
+              name: "Dr. Deepthi Sasidharan",
+              designation: "Chair",
+              image: Deepthi,
+              linkedin: "https://www.linkedin.com/in/deepthi-sasidharan-26330b7/",
+              mail:"",
+              mobNo:""
+          },
+          {
+              name: "Prof. Sarju S",
+              designation: "Vice Chair",
+              image: Sarju,
+              linkedin: "https://in.linkedin.com/in/sarju-s",
+              mail:"",
+              mobNo:""
+          },
+          {
+              name: "Mr. Akhil Raj",
+              designation: "Secretary",
+              image:Akhil,
+              linkedin: "https://www.linkedin.com/in/1akhil-raj",
+              mail:"",
+              mobNo:""
+          },
+          {
+              name: "Mr. Karthik K",
+              designation: "Treasurer",
+              image:Karthik,
+              linkedin: "https://in.linkedin.com/in/karthik1997",
+              mail:"",
+              mobNo:""
+          },
+          {
+              name: "Dr. R.Satheesh Kumar",
+              designation: "Joint Secretary, Academia",
+              image: Satheesh,
+              linkedin: "https://www.linkedin.com/in/dr-r-satheesh-kumar-80956a118/",
+              mail:"",
+              mobNo:""
+          },
+  
+          {
+              name: "Mr. Janajith D",
+              designation: "Joint Secretary, YP",
+              image:Janajith,
+              linkedin: "https://www.linkedin.com/in/janajithd/",
+              mail:"",
+              mobNo:""
+          },
+          
+          {
+              name: "Mr. Sabareesh K",
+              designation: "Joint Secretary, Industry",
+              image:Sabareesh,
+              linkedin: "https://www.linkedin.com/in/sabareeshk/",
+              mail:"",
+              mobNo:""
+          },
+                  
+          {
+              name: "Ms. Alia Shiraz",
+              designation: "Joint Secretary, MD",
+              image: Shiraz,
+              linkedin: "https://www.linkedin.com/in/alia-shiraz/",
+              mail:"",
+              mobNo:""
+          },
+                  
+          {
+              name: "Mr. Justin Jacob",
+              designation: "Joint Secretary, PR",
+              image: Justin,
+              linkedin: "https://www.linkedin.com/in/justinjacob01/",
+              mail:"",
+              mobNo:""
+          },
+  
+          {
+              name: "Mr. Devanarayanan",
+              designation: "Student Representative",
+              image:Devanarayanan,
+              linkedin: "https://in.linkedin.com/in/devanarayanansv",
+              mail:"",
+              mobNo:""
+          },
+          {
+              name: "Ms. Anna Ann Mathew",
+              designation: "Technical Coordinator",
+              image:AnnaMathew,
+              linkedin: "https://www.linkedin.com/in/anna-mathew-b10572214",
+              mail:"",
+              mobNo:""
+          },
+          {
+              name: "Mr.Biswas Sekhar",
+              designation: "Electronic Communication Coordinator",
+              image: Biswas,
+              linkedin: "https://www.linkedin.com/in/biswas-sekhar/",
+              mail:"",
+              mobNo:""
+          },
+          {
+              name: "Ms. Neha Arun",
+              designation: "Women in Computing Coordinator",
+              image: Neha,
+              linkedin: "https://www.linkedin.com/in/neha-arun-452252228/",
+              mail:"",
+              mobNo:""
+          },
+          {
+              name: "Ms. Maria Tresa Binu",
+              designation: "Travancore Hub Student Representative",
+              image: Maria,
+              linkedin: "https://www.linkedin.com/in/maria-tresa-binu-a23722241",
+              mail:"",
+              mobNo:""
+          },
+          {
+              name: "Mr. Robin Francis",
+              designation: "Kochi Hub Student Representative",
+              image:Robin,
+              linkedin: "https://www.linkedin.com/in/robin-francis-b43565175/",
+              mail:"",
+              mobNo:""
+          },
+          {
+              name: "Mr. Nathan Jose",
+              designation: "Malabar Hub Student Representative",
+              image:Nathan,
+              linkedin: "http://linkedin.com/in/natjoe",
+              mail:"",
+              mobNo:""
+          },
+          {
+              name: "Mr. Abhishek S",
+              designation: "Webmaster",
+              image: Abhishek,
+              linkedin: "https://www.linkedin.com/in/abhishek-santhosh",
+              mail:"",
+              mobNo:""
+          },
+          {
+              name: "Mr. Vasif Abdulla P",
+              designation: "Webmaster",
+              image:Vasif,
+              linkedin: "https://www.linkedin.com/in/vasifabdulla",
+              mail:"",
+              mobNo:""
+          },
+          {
+              name: "Mr. Swsthik H Nair",
+              designation: "Entrepreneurship and Innovation Coordinator",
+              image:Swsthik,
+              linkedin: "https://www.linkedin.com/in/swsthik-h-nair-26a451249",
+              mail:"",
+              mobNo:""
+          },
+          {
+              name: "Mr. Jyotish T R",
+              designation: "Special Interest Group AI Lead",
+              image: Jyotish,
+              linkedin: "https://www.linkedin.com/in/jyothishtr2001",
+              mail:"",
+              mobNo:""
+          },
+          {
+              name: "Ms. Abhirami P",
+              designation: "Membership Development Coordinator",
+              image: Abhirami,
+              linkedin: "https://www.linkedin.com/in/abhirami-p-98209a22a/",
+              mail:"",
+              mobNo:""
+          }
+      ];
