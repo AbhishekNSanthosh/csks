f99b9c1302c6f06882f2cc309994a8c214f73fd8
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-06-08 19:45:01 +0530

    refactor: about us changed

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -32,6 +32,52 @@ export const AboutUsComponent = () => {
             <img src={about} alt="" className={styles.img} />
           </div>
         </div>
+
+        <div className={styles.wrap}>
+          <div className={styles.left}>
+            <span className={styles.subTitle}>The Professional Execom</span>
+            <p className={styles.desc}>
+              The <b>Professional Execom</b> stands as a beacon of visionary
+              leadership and unwavering dedication within the IEEE CS Kerala
+              Chapter, guiding every endeavor with precision and expertise. The
+              collective wisdom,composed of efficient professionals,forms the
+              bedrock of our chapter's aspirations, propelling us towards
+              unparalleled excellence. With their refined professionalism and
+              strategic guidance, they guarantee the triumph and elevated
+              standards of each initiative we undertake.
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.wrap}>
+          <div className={styles.left}>
+            <span className={styles.subTitle}>The IEEE CS SYP</span>
+            <p className={styles.desc}>
+              The<b> IEEE CS SYP</b> is a dynamic hub of support and inspiration
+              for emerging computer scientists. Our dedicated team empowers
+              students and early-career graduates with exceptional resources and
+              opportunities to excel both academically and professionally.
+              Guided by visionary leadership and an unwavering commitment to
+              excellence, we cultivate a thriving community where innovation
+              flourishes, shaping the future of computer science.
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.wrap}>
+          <div className={styles.left}>
+            <span className={styles.subTitle}>The IEEE CS SLT</span>
+            <p className={styles.desc}>
+            The<b> IEEE CS SLT</b> functions as the nexus for all student activities
+              within our chapter. With their exceptional leadership, they
+              provide unwavering support and ensure access to all necessary
+              resources, fostering an environment conducive to excellence.
+              Acting as the cornerstone of guidance, the SLT upholds the highest
+              standards in every activity, thereby guaranteeing a thriving
+              atmosphere where student success flourishes.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
